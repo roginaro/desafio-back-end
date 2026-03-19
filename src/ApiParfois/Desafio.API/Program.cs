fdbfc27e02775e1ac11da90d3cf71c8e2810117d
Igor Souza <rogicavalcante@gmail.com>
2026-03-19 19:45:49 +0000

    feat: finaliza configuração da API com DI e Swagger

--- src/ApiParfois/Desafio.API/Program.cs
+++ src/ApiParfois/Desafio.API/Program.cs
@@ -1,17 +1,31 @@
+using Desafio.Application.Services;
+using Desafio.Domain.Interfaces;
+using Desafio.Infrastructure.Data;
+using Desafio.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("DesafioParfoisDb"));
+
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<IPedidoService, PedidoService>();
+builder.Services.AddScoped<IStatusService, StatusService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
